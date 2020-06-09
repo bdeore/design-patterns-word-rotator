49240fcc17d232cf230402c817410c444b120c55
Bhagwan Deore <deoresuyog@gmail.com>
2020-06-09 05:07:30 -0400

    Finished MetricsCalculator class driver code is for testing purposes only.

--- wordPlay/src/wordPlay/driver/Driver.java
+++ wordPlay/src/wordPlay/driver/Driver.java
@@ -1,22 +1,62 @@
 package wordPlay.driver;
 
-/**
- * @author Bhagwan Deore
- *
- */
+import java.util.Vector;
+import wordPlay.metrics.MetricsCalculator;
+import wordPlay.util.FileProcessor;
+
+/** @author Bhagwan Deore */
 public class Driver {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		/*
-		 * As the build.xml specifies the arguments as input,output or metrics, in case the
-		 * argument value is not given java takes the default value specified in
-		 * build.xml. To avoid that, below condition is used
-		 */
-		if ((args.length != 3) || (args[0].equals("${input}")) || (args[1].equals("${output}")) || (args[2].equals("${metrics}"))) {
-			System.err.println("Error: Incorrect number of arguments. Program accepts 3 arguments.");
-			System.exit(0);
-		}
-		System.out.println("Hello World! Lets get started with the assignment");
+    /*
+     * As the build.xml specifies the arguments as input,output or metrics, in case the
+     * argument value is not given java takes the default value specified in
+     * build.xml. To avoid that, below condition is used
+     */
+    if ((args.length != 3)
+        || (args[0].equals("${input}"))
+        || (args[1].equals("${output}"))
+        || (args[2].equals("${metrics}"))) {
+      System.err.println("Error: Incorrect number of arguments. Program accepts 3 arguments.");
+      System.exit(0);
+    }
+    System.out.println("Hello World! Lets get started with the assignment");
 
-	}
+    try {
+      System.out.println(args[0]);
+      FileProcessor fp = new FileProcessor(args[0]);
+
+      String temp = fp.poll();
+      while (temp != null) {
+        System.out.println(temp);
+        temp = fp.poll();
+      }
+
+    } catch (Exception e) {
+      System.out.println("exception occurred in FileProcessor class");
+      e.printStackTrace();
+    }
+
+    Vector<Integer> vec = new Vector<Integer>();
+    vec.add(7);
+    vec.add(2);
+    vec.add(6);
+    vec.add(8);
+    vec.add(6);
+    vec.add(4);
+    MetricsCalculator newTest = new MetricsCalculator();
+    newTest.addLineStats(0, vec);
+
+    vec = new Vector<Integer>();
+    vec.add(5);
+    vec.add(7);
+    vec.add(2);
+    vec.add(4);
+    vec.add(10);
+    vec.add(7);
+    newTest.addLineStats(1, vec);
+    newTest.printVector();
+    System.out.println(newTest.getAvgWordsPerSentence());
+    System.out.println(newTest.getAvgWordLength());
+  }
 }
--- wordPlay/src/wordPlay/metrics/MetricsCalculator.java
+++ wordPlay/src/wordPlay/metrics/MetricsCalculator.java
@@ -1,5 +1,68 @@
 package wordPlay.metrics;
 
+import java.util.Vector;
+
 public class MetricsCalculator {
 
+  private double avgWordsPerSentence;
+  private double avgWordLength;
+
+  private Vector<Vector<Integer>> metrics;
+
+  public MetricsCalculator() {
+    this.avgWordsPerSentence = 0;
+    this.avgWordLength = 0;
+    metrics = new Vector<Vector<Integer>>();
+  }
+
+  public double getAvgWordsPerSentence() {
+    calculateAvgWordsPerSentence();
+    return avgWordsPerSentence;
+  }
+
+  public double getAvgWordLength() {
+    calculateAvgWordLength();
+    return avgWordLength;
+  }
+
+  public void addLineStats(int lineNumber, Vector<Integer> elements) {
+    if (lineNumber >= 0 && elements.size() > 0) {
+      metrics.add(lineNumber, elements);
+    }
+  }
+
+  public void printVector() {
+    for (Vector<Integer> tempReference : metrics) {
+      for (Integer integer : tempReference) {
+        System.out.print(integer + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  private void calculateAvgWordsPerSentence() {
+    int size = 0;
+    for (Vector<Integer> tempReference : metrics) {
+      size += tempReference.size();
+    }
+    avgWordsPerSentence = (double) size / metrics.size();
+    avgWordsPerSentence = roundUp(avgWordsPerSentence);
+  }
+
+  private void calculateAvgWordLength() {
+    int charCount = 0, wordCount = 0;
+    for (Vector<Integer> tempReference : metrics) {
+      wordCount += tempReference.size();
+      for (Integer integer : tempReference) {
+        charCount += integer;
+      }
+    }
+    avgWordLength = (double) charCount / wordCount;
+    avgWordLength = roundUp(avgWordLength);
+    // avgWordLength = (double) ((int)(avgWordLength * 100)) / 100;
+  }
+
+  private double roundUp(double value) {
+    return ((double) Math.round(value * 100) / 100);
+  }
 }
